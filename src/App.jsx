c188971a8560e8a60944dfb7d9073534e0392387
Yurii Bolharyn <yurecpr@gmail.com>
2024-04-17 19:38:41 +0200

    Lesson02

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css';
+import Lesson02 from './lessons/Lessons02/Lessons02';
+
+
+function App() {
+  return (<div className="App">
+  <Lesson02/>
+  </div>);
+}
+
+export default App;
